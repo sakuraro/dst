6f66f6f2882d72fc5af1f0b77e848542dddf4e07
sakura <tomeetyouismyfortune@gmail.com>
2026-03-06 15:26:31 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM debian:13
+ADD dst_servers.sh /root/
+
+WORKDIR /root
+RUN chmod u+x ./dst_servers.sh \
+    && ./dst_servers.sh install
+
+CMD ["./dst_servers.sh", "run", "Master"]
---
+++ dst_servers.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+steamcmd_dir="$HOME/steamcmd"
+install_dir="$HOME/dontstarvetogether_dedicated_server"
+cluster_name="MyDediServer"
+dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
+
+function fail()
+{
+	echo Error: "$@" >&2
+	exit 1
+}
+
+function check_for_file()
+{
+	if [ ! -e "$1" ]; then
+		fail "Missing file: $1"
+	fi
+}
+
+function install()
+{
+    # install dependence
+    dpkg --add-architecture i386
+    apt update
+    apt install -y wget libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386
+
+    # install game
+    mkdir -p $steamcmd_dir
+    cd $steamcmd_dir
+    wget -qO - 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar -xvzf -
+    chmod u+x ./steamcmd.sh
+    ./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+}
+
+function run()
+{
+    # run game
+    check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
+    check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
+    check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
+    check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
+
+    cd "$install_dir/bin64"
+    run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
+    run_shared+=(-console)
+    run_shared+=(-cluster "$cluster_name")
+    run_shared+=(-monitor_parent_process $$)
+
+    "${run_shared[@]}" -shard $1  | sed "s/^/$1:  /"
+}
+
+if [ "$1" == "install" ]; then
+    install
+elif [ "$1" == "run" ]; then
+    run $2
+else
+    echo "无效参数"
+fi
